944ac24732f0a0573a3803966cd4ec0541110a95
Jazaeri, Mahdi <m.jazaeri@daal.co>
2024-12-25 01:45:06 +0330

    [feat] Added order management endpoints for admin

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { placeOrder } from "../controllers/orderController";
+import { placeOrder, getAllOrders, getOrderById, updateOrderStatus } from "../controllers/orderController";
 import { protect, authorizedRoles } from "../middlewares/authMiddleware";
 
 
@@ -8,6 +8,19 @@ const router = express.Router();
 
 // Place order
 router.post('/', protect, placeOrder)
+
+
+// Get all orders - Admin
+router.get('/', protect, authorizedRoles("ADMIN"), getAllOrders);
+
+
+// Get an order by ID - Admin
+router.get('/:id', protect, authorizedRoles("ADMIN"), getOrderById);
+
+
+// Update order status
+router.put('/:id/status', protect, authorizedRoles("ADMIN"), updateOrderStatus);
+
 
 
 export default router;
